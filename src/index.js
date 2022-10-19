55ccf256cd6986f3cd1ce96dfe7f94578ca8be9d
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2022-10-20 02:27:53 +0300

    change calendar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import animation from "./scripts/animation";
 import "./style.css";
+import 'flatpickr/dist/flatpickr.css';
 
 animation();
--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,20 +1,20 @@
-import AirDatepicker from "air-datepicker";
-import "air-datepicker/air-datepicker.css";
+import flatpickr from "flatpickr";
 import animation from "./animation";
 
-const calendar = (mark, model, year, dates) => {
+function calendar(mark, model, year, dates) {
   const minDate = new Date(dates[0]);
   const maxDate = new Date(dates[1]);
 
-  new AirDatepicker("#input", {
-    isMobile: true,
-    autoClose: true,
+  const flatpickrCalendar = flatpickr("#input", {
+    disableMobile: "true",
     minDate,
     maxDate,
-    onSelect({ formattedDate }) {
-      showData(formattedDate);
+    onClose() {
+      const data = this.selectedDates[0];
+      console.log(data.toLocaleDateString());
+      showData(data.toLocaleDateString());
       restart();
-    },
+    }
   });
 
   const showData = (data) => {
@@ -25,12 +25,13 @@ const calendar = (mark, model, year, dates) => {
   };
 
   const restart = () => {
-    const restartButton = document.querySelector(".restart-button");
-    restartButton.addEventListener("click", () => {
-      document.body.innerHTML = "";
+    const restartButton = document.querySelector('.restart-button');
+    restartButton.addEventListener('click', () => {
+      document.body.innerHTML = '';
       animation();
     });
   };
-};
+  return flatpickrCalendar;
+}
 
 export default calendar;
